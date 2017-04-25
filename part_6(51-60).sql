a49e2d269b62f9e71a74512f235118331639014b
hedgehogus <research162@gmail.com>
2017-04-25 23:35:11 +0300

    59

--- part_6(51-60).sql
+++ part_6(51-60).sql
@@ -149,5 +149,26 @@ WHEN e.summ =0 THEN 0.00
 FROM f INNER JOIN e
 ON f.maker = e.maker
 
+-- 59. Calculate the cash balance of each buy-back center for the database with money transactions being recorded not more than once a day.
+-- Result set: point, balance.
+
+WITH a AS (SELECT point,
+SUM(inc) as income
+FROM Income_o
+GROUP BY point),
+b AS (SELECT point, SUM(out) as outcome
+FROM Outcome_o
+GROUP BY point),
+c as (SELECT a.point AS point,
+CASE WHEN income IS NULL THEN 0
+ELSE income END income,
+CASE WHEN outcome IS NULL THEN 0
+ELSE outcome END outcome
+FROM a FULL JOIN b
+ON a.point = b.point)
+SELECT point, income-outcome as remain
+FROM c
+
+
 
 
