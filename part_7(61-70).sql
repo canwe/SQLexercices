1bf0bef81ae3607479fe16257a89b133a0041ee2
hedgehogus <research162@gmail.com>
2017-05-12 16:45:04 +0300

    62

--- part_7(61-70).sql
+++ part_7(61-70).sql
@@ -1,4 +1,4 @@
--- For the database with money transactions being recorded not more than once a day, calculate the total cash balance of all buy-back centers.
+-- 61. For the database with money transactions being recorded not more than once a day, calculate the total cash balance of all buy-back centers.
 
 WITH a AS (SELECT point,
 SUM(inc) as income
@@ -6,6 +6,28 @@ FROM Income_o
 GROUP BY point),
 b AS (SELECT point, SUM(out) as outcome
 FROM Outcome_o
+GROUP BY point),
+c as (SELECT a.point AS point,
+CASE WHEN income IS NULL THEN 0
+ELSE income END income,
+CASE WHEN outcome IS NULL THEN 0
+ELSE outcome END outcome
+FROM a FULL JOIN b
+ON a.point = b.point)
+SELECT SUM(income-outcome) as remain
+FROM c;
+
+-- 62. For the database with money transactions being recorded not more than once a day,
+-- calculate the total cash balance of all buy-back centers at the beginning of 04/15/2001.
+
+WITH a AS (SELECT point,
+SUM(inc) as income
+FROM Income_o
+WHERE date < '2001-04-15'
+GROUP BY point),
+b AS (SELECT point, SUM(out) as outcome
+FROM Outcome_o
+WHERE date < '2001-04-15'
 GROUP BY point),
 c as (SELECT a.point AS point,
 CASE WHEN income IS NULL THEN 0
